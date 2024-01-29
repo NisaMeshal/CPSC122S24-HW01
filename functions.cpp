9adefacdd2792ba666ef40035a88369bb4c2e5d0
Nisa Meshal <nisameshal@gmail.com>
2024-01-29 12:53:55 -0800

    header and function files created

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+#include "header.h"
+
+/*
+    TODO: implement this function to read a string from the terminal
+*/
+string readString() {
+
+}
+
+/*
+    TODO: implement this function to find the vowels in the inputString and insert them into the vowels vector 
+*/
+void findVowels(string inputString, vector<char> vowels) {
+
+}
---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H 
+#define FUNCTIONS_H
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std; 
+
+string readString();
+void findVowels(string inputString, vector<char> vowels);
+
+#endif
